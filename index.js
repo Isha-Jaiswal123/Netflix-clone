51d6b5a6ab25f38b0c61e5a7f1d3d2504929fcfe
Isha-Jaiswal123 <ishujaiswal746@gmail.com>
2024-01-14 15:46:31 +0530

    add inisial files

---
+++ index.js
@@ -0,0 +1,72 @@
+
+let answer=document.querySelector(".ans1");
+let show=true;
+function run(){
+    if(show){
+      answer.style.display="block";
+      show=false;
+    }else{
+        answer.style.display="none";
+        show=true;
+    }
+}
+
+let answer2=document.querySelector(".ans2");
+let show2=true;
+function run2(){
+    if(show2){
+      answer2.style.display="block";
+      show2=false;
+    }else{
+        answer2.style.display="none";
+        show2=true;
+    }
+}
+
+let answer3=document.querySelector(".ans3");
+let show3=true;
+function run3(){
+    if(show3){
+      answer3.style.display="block";
+      show3=false;
+    }else{
+        answer3.style.display="none";
+        show3=true;
+    }
+}
+
+let answer4=document.querySelector(".ans4");
+let show4=true;
+function run4(){
+    if(show4){
+      answer4.style.display="block";
+      show4=false;
+    }else{
+        answer4.style.display="none";
+        show4=true;
+    }
+}
+
+let answer5=document.querySelector(".ans5");
+let show5=true;
+function run5(){
+    if(show5){
+      answer5.style.display="block";
+      show5=false;
+    }else{
+        answer5.style.display="none";
+        show5=true;
+    }
+}
+
+let answer6=document.querySelector(".ans6");
+let show6=true;
+function run6(){
+    if(show6){
+      answer6.style.display="block";
+      show6=false;
+    }else{
+        answer6.style.display="none";
+        show6=true;
+    }
+}
